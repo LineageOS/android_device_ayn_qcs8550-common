25908dac3ff4e24f1ab6b34264cbde51fb65ce39
AnierinB <anierin@evolution-x.org>
2025-01-08 22:02:26 +0100

    qcs8550-common: Import fstab from stock and adapt
    Change-Id: I3e9da15f4b28627516303c8328d39a45f7eaec50
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- common.mk
+++ common.mk
@@ -37,6 +37,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    fstab.default \
+    fstab.default.vendor_ramdisk
+
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
