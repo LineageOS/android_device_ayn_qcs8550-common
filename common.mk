c196ae515f0193190fd4a8c8e9a3ca36c2ba379f
LuK1337 <priv.luk@gmail.com>
2025-01-18 20:12:43 +0100

    qcs8550-common: overlay: Configure lineage-sdk
    Change-Id: I343b41c376be51d17d46e9f8c25d6a5bed27121f

--- common.mk
+++ common.mk
@@ -190,6 +190,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResCommon \
