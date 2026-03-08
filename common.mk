3ddf5c24e502d040477f8ee1e7afdca3ea1eb88b
AnierinB <anierin@evolution-x.org>
2026-03-08 21:36:12 +0000

    qcs8550-common: Include keymint
    Change-Id: I4cc79f7e9941ed381bdbe7c4e3b283f0d9600c37
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- common.mk
+++ common.mk
@@ -116,6 +116,14 @@ PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_COPY_FILES += \
     kernel/ayn/qcs8550/modules.systemdlkm_blocklist.msm.kalama:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
 
+# Keymint
+PRODUCT_PACKAGES += \
+    android.hardware.hardware_keystore.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
+    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+
 # Memtrack
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
