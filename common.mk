e8fd9a1e0c45b7903fcc615677ca6eb3f429b71b
AnierinB <anierin@evolution-x.org>
2026-03-13 07:51:16 +0000

    qcs8550-common: Build AynParts & configure common features
    Change-Id: Ic918135d66a1d337520de3d25b9cdde5b16c57c6
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- common.mk
+++ common.mk
@@ -81,6 +81,10 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
+# AynParts
+PRODUCT_PACKAGES += \
+    AynParts
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     lib_bt_aptx \
@@ -162,6 +166,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.target.rc \
+    ueventd.ayn.rc \
     ueventd.qcom.rc
 
 # Keymint
@@ -189,6 +194,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
+    AynPartsOverlayCommon \
     FrameworksResCommon \
     FrameworksResTarget \
     LineageSDKOverlayCommon \
@@ -238,7 +244,8 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/ayn
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
